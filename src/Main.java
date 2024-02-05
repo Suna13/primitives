0d4c04704fe85096e338df63eef059411028e8c2
Sulyma Anastasia <sulyma13@mail.ru>
2024-02-05 22:14:26 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 100565;
+        int miles = 20;
+        int bonus = price/miles;
+        System.out.println(bonus);
+    }
+}
